d488f1f58998b459b6581a21a83b8cf35d86213b
xbmlz <xbmlz@users.noreply.github.com>
2022-11-25 16:59:27 +0000

    deploy: e51d9a7222470afcfb32d3eab4bdfd606d5a8f27

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// do something
+// open-menu
+$(document).ready(function () {
+  $("#open-menu").click(function () {
+    // set height auto
+    $("#menu-panel").css("height", "auto");
+    // set translate y 0
+    $("#menu-content").css(
+      "transform",
+      "translate(0, 0) rotate(0) skew(0) scaleX(1) scaleY(1)"
+    );
+    $("#open-menu").css("display", "none");
+    $("#close-menu").css("display", "block");
+  });
+
+  $("#close-menu").click(function () {
+    // set height 0
+    $("#menu-panel").css("height", "0");
+    // set translate y -100%
+    $("#menu-content").css(
+      "transform",
+      "translate(0, -100%) rotate(0) skew(0) scaleX(1) scaleY(1)"
+    );
+    $("#open-menu").css("display", "block");
+    $("#close-menu").css("display", "none");
+  });
+});
