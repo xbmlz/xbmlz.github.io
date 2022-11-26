f1745d3e915b2e2dff944f8db3d995a1cb7969ec
Petrichor <997909544@qq.com>
2022-11-26 12:54:33 +0800

    feat: ad go up

--- themes/maple/source/js/index.js
+++ themes/maple/source/js/index.js
@@ -29,13 +29,39 @@ $(document).ready(function () {
     function (entries) {
       entries.forEach(function (entry) {
         if (entry.intersectionRatio > 0) {
-          $("#header-title").css("display", "none").css("opacity", "0");
+          $("#header-title")
+          .css("opacity", "0")
+          .css("transform", "translate(0, -100%)")
+          .css("transition", "all 0.3s");
         } else {
-          $("#header-title").css("display", "block").css("opacity", "1");
+          $("#header-title")
+          .css("opacity", "1")
+          .css("transform", "translate(0, 0)")
+          .css("transition", "all 0.3s");
         }
       });
     }
     // { threshold: [0, 0.25, 0.5, 0.75, 1] }
   );
   observer.observe(document.querySelector("#article-title"));
+
+  // go up show
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 200) {
+      // 添加动画过度
+      $("#go-up")
+        .css("opacity", "1")
+        .css("transform", "translateX(0) rotate(0)")
+        .css("transition", "all 0.3s");
+    } else {
+      $("#go-up")
+        .css("opacity", "0")
+        .css("transform", "translateX(50px) rotate(180deg)")
+        .css("transition", "all 0.3s");
+    }
+  });
+  // go up
+  $("#go-up").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 500);
+  });
 });
