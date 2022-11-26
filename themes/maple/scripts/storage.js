485f3ed63806c3ad5f5f914b74dbd347bcc092c4
Petrichor <997909544@qq.com>
2022-11-27 01:47:21 +0800

    feat: add lizhi

--- themes/maple/scripts/storage.js
+++
@@ -1,8 +0,0 @@
-// 注册localStorage方法
-// 使用方式 <%= storage('key') %>
-hexo.extend.helper.register('storage', function (key, value) {
-  if (value) {
-    return localStorage.setItem(key, value);
-  }
-  return localStorage.getItem(key);
-})
